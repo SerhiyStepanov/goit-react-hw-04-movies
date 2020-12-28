5345cd6781eeb8bc602c4489b34b75a441efab2e
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-12-28 12:35:37 +0200

    add normalized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const KEY = "7b5c027beb1723358e7047ebab76ed47";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>goit-react-hw-04-movies</h3>
+    </>
   );
 }
-
-export default App;
